f8afa763314a4ecdcd624320d7ebe52db1c595b4
juanfradelrm <juanfradelrm@gmail.com>
2025-04-01 18:59:22 +0200

    feat: FireBase Services added.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import {provideHttpClient} from '@angular/common/http';
-
 import { routes } from './app.routes';
 import { environment } from '../environments/environments';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 
-
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig))
   ]
 };
