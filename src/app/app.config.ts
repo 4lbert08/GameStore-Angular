c46ddfcfd472e68cc059ea1b5201cd3e0f01f31e
Alberto José Rodríguez Ruano <alberto.rodriguez.2708@gmail.com>
2025-04-01 17:39:20 +0100

    feat: Firebase added.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {provideHttpClient} from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { environment } from '../environments/environments';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig))
+  ]
 };
---
+++ src/environments/environments.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBSlLlZMA_I8VLNXpOCJZUBqypcGJbUsIg",
+    authDomain: "gamestore-cc423.firebaseapp.com",
+    projectId: "gamestore-cc423",
+    storageBucket: "gamestore-cc423.firebasestorage.app",
+    messagingSenderId: "418332427271",
+    appId: "1:418332427271:web:01cc467786ac5e420916a2"
+  }
+};
+
