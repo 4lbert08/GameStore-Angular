155d966418c57258b111f4dafeb1b1725a55cb38
juanfradelrm <juanfradelrm@gmail.com>
2025-04-03 01:14:44 +0200

    Auth Service added.

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, User, onAuthStateChanged } from 'firebase/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private auth = getAuth();
+  user$: Observable<User | null>;
 
-  constructor() { }
+  constructor() {
+    this.user$ = new Observable((subscriber) => {
+      onAuthStateChanged(this.auth, (user) => {
+        subscriber.next(user);
+      });
+    });
+  }
+
+  async login(email: string, password: string): Promise<User | null> {
+    try {
+      const userCredential = await signInWithEmailAndPassword(this.auth, email, password);
+      return userCredential.user;
+    } catch (error) {
+      console.error("Error in login:", error);
+      return null;
+    }
+  }
+
+  async register(email: string, password: string): Promise<User | null> {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(this.auth, email, password);
+      return userCredential.user;
+    } catch (error) {
+      console.error("Error in register:", error);
+      return null;
+    }
+  }
+
+  async logout(): Promise<void> {
+    try {
+      await signOut(this.auth);
+    } catch (error) {
+      console.error("Error in logout", error);
+    }
+  }
+
+  getCurrentUser(): Observable<User | null> {
+    return this.user$;
+  }
 }
