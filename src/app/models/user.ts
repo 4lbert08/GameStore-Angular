507ad4bbaa55ceaecf550a7a2cf88f3f45cbd10b
juanfradelrm <juanfradelrm@gmail.com>
2025-04-05 21:07:50 +0200

    The in-cart and review game components have been refactored, and the services and models required for reviews have been created.

---
+++ src/app/models/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+  id: string;
+  image: string;
+  nickname: string;
+  birthday: Date;
+  email: string;
+  region: string;
+  language: string;
+}
--- src/app/services/firestore/firestore.service.ts
+++ src/app/services/firestore/firestore.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
-import {collection, collectionData, doc, docData, Firestore} from '@angular/fire/firestore';
+import {collection, collectionData, doc, docData, Firestore, query, where} from '@angular/fire/firestore';
 import {Observable} from 'rxjs';
 import {Game} from '../../models/game';
 import {Developer} from '../../models/developer';
+import {Review} from '../../models/review';
+import {User} from '../../models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -25,4 +27,24 @@ export class FirestoreService {
     const developerRef = collection(this.firestore, 'developers');
     return collectionData(developerRef, { idField: 'id' }) as Observable<Developer[]>;
   }
+
+  getUserById(userId: string): Observable<User> {
+    const userDocRef = doc(this.firestore, `users/${userId}`);
+    return docData(userDocRef, { idField: 'id' }) as Observable<User>;
+  }
+
+  getReviewsFromGame(gameId: string): Observable<Review[]> {
+    return collectionData(
+      query(collection(this.firestore, 'reviews'), where('gameId', '==', gameId)),
+      { idField: 'id' }
+    ) as Observable<Review[]>;
+  }
+
+  getReviewsFromUser(userId: string): Observable<Review[]> {
+    return collectionData(
+      query(collection(this.firestore, 'reviews'), where('userId', '==', userId)),
+      { idField: 'id' }
+    ) as Observable<Review[]>;
+  }
+
 }
