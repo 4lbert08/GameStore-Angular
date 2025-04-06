be90b0f6f22e896018b0da5c9152a2ed0837aee7
Juanelboi <juangarciaboissier@gmail.com>
2025-04-06 20:53:44 +0100

    feat: header and footer added

---
+++ src/app/components/secondary-header/secondary-header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-secondary-header',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './secondary-header.component.html',
+  styleUrl: './secondary-header.component.css'
+})
+export class SecondaryHeaderComponent {
+
+}
